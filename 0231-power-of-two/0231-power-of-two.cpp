7bf1f23160f6ee18b074dac7ee285f6c1035c225
Adr <52234332+kumarrahulalld@users.noreply.github.com>
2024-02-19 20:32:22 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (39.77%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<0)
+        {
+            return false;
+        }
+        return __builtin_popcount(n)==1;
+    }
+};
